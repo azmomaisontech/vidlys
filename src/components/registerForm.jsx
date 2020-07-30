d5577983020d13803ef07d2cd2662396fd0bae6f
AZMOMAISON <mosesaizee@outlook.com>
2020-07-30 06:51:27 -0500

    Refactoring the registerForm from class to functional component

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,45 +1,25 @@
-import React from "react";
-import Joi from "joi-browser";
-import Form from "./common/form";
+import React, { useState } from "react";
 
-class RegisterForm extends Form {
-  state = {
-    data: { username: "", password: "", name: "" },
-    errors: {}
-  };
+const RegisterForm = () => {
+  const [data, setData] = useState({ username: "", password: "", name: "" });
 
-  schema = {
-    username: Joi.string()
-      .required()
-      .email()
-      .label("Username"),
-    password: Joi.string()
-      .required()
-      .min(5)
-      .label("Password"),
-    name: Joi.string()
-      .required()
-      .label("Name")
-  };
-
-  doSubmit = () => {
+  const doSubmit = () => {
     // Call the server
     console.log("Submitted");
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Register</h1>
-        <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderInput("name", "Name")}
-          {this.renderButton("Register")}
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={doSubmit}>
+        <input type="text" />
+        {this.renderInput("username", "Username")}
+        {this.renderInput("password", "Password", "password")}
+        {this.renderInput("name", "Name")}
+        {this.renderButton("Register")}
+      </form>
+    </div>
+  );
+};
 
 export default RegisterForm;
